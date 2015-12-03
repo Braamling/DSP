25d617f905c4432c75393e373af63e504c87790e
Wouter <wouter-vrielink@hotmail.com>
2015-12-02 20:49:21 -0500

    yes?

--- shazam.py
+++ shazam.py
@@ -13,18 +13,19 @@ print fft_fs
 print fs
 
 a = data.T[0]
-#for i in range(length * times):
-#    print fft_fs*(i+1)
-#    # two channel track, get the first channel
+for i in range(length * times):
+    print fft_fs*(i+1)
+    # two channel track, get the first channel
 
 
-#    # 8-bit track, b is now normalized on [-1,1)
-#    #b=[(x/2**8.)*2-1 for x in a] 
-#    
-#    c = fft(a[i*fft_fs:fft_fs*(i+1)]) # calculate fourier transform (complex numbers list)
-#    d = len(c)/2  # you only need half of the fft list (real signal symmetry)
-#    plt.plot(abs(c[:(d-1)]),'r') 
-#    plt.show()
+    # 8-bit track, b is now normalized on [-1,1)
+    #b=[(x/2**8.)*2-1 for x in a] 
+    
+    c = fft(a[i*fft_fs:fft_fs*(i+1)]) # calculate fourier transform (complex numbers list)
+    d = len(c)/2  # you only need half of the fft list (real signal symmetry)
+    X[i,:] = np.log(np.abs(z[:nfft/2]))
+    plt.plot(abs(c[:(d-1)]),'r') 
+    plt.show()
     
 from pylab import *
 specgram(b)
