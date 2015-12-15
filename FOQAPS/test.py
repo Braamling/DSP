e7fef6c5886e95547a920bc8b5fb24cff898f1f8
Braamling <contact@bramvandenakker.nl>
2015-12-15 16:29:16 +0100

    realtime lfilter

--- FOQAPS/test.py
+++ FOQAPS/test.py
@@ -1,73 +1,134 @@
+"""
+authors: Wouter Vrielink & Bram van den Akker
+date: 15-12-2015
+
+
+"""
+
 import alsaaudio
 import struct
 import time
+
 from scipy import signal
+import numpy as np
 
 class FOQAPS():
-
-    """ Initialize input and output streams """
     def __init__(self):
-        channels = 1
+        """ Initialize input and output streams """
+        self.channels = 1
         sample_size = 1
-        self.frame_size = channels * sample_size
-        frame_rate = 44100
-        byte_rate = frame_rate * self.frame_size
-        self.period_size = 1024
+        self.frame_size = self.channels * sample_size
+        self.frame_rate = 44100
+        byte_rate = self.frame_rate * self.frame_size
+        self.period_size = 320
 
-        # Create output object
-        self.out = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK, alsaaudio.PCM_NORMAL)
-         
-        # Configure the output stream
-        self.out.setchannels(channels)
-        self.out.setformat(alsaaudio.PCM_FORMAT_FLOAT_LE)
-        self.out.setrate(frame_rate)
-        self.out.setperiodsize(self.period_size)
+        self.create_output_stream()
+        self.create_input_stream()
+
+    def create_input_stream(self):
+        """ Create an input stream and store it in the object.
+        """
 
         # Create input object
         self.inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL)
         
         # Configure the input stream
-        self.inp.setchannels(channels)
-        self.inp.setrate(frame_rate)
+        self.inp.setchannels(self.channels)
+        self.inp.setrate(self.frame_rate)
         self.inp.setformat(alsaaudio.PCM_FORMAT_FLOAT_LE)#16 bit little endian
         self.inp.setperiodsize(self.period_size)
+
+    def create_output_stream(self):
+        """ Create an output stream and store it in the object """
+        self.out = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK, alsaaudio.PCM_NORMAL)
          
+        # Configure the output stream
+        self.out.setchannels(self.channels)
+        self.out.setformat(alsaaudio.PCM_FORMAT_FLOAT_LE)
+        self.out.setrate(self.frame_rate)
+        self.out.setperiodsize(self.period_size)
+
          
-    def foqaps_butter(self, order, freq , data, filter_type = 'lowpass'):
+    def execute_filter(self, b, a, buffer, pre_buffer):
         """ Order; the order of the filter (int). 
         Freq; scalar or length-2 sequence giving the critical frequencies.
         Data; N-dimensional input array.
         Filter_type: {'lowpass', 'highpass', 'bandpass', 'bandstop'}, optional.
         """
-        a, b = signal.butter(order, freq, btype = filter_type, analog=False)
-        return signal.lfilter(b, a, data)
+        timing = time.time()
+        pre_buffer = np.concatenate([pre_buffer, buffer])
 
-    def playback_audio_realtime(self, end_time):
-        run_time = 0
-        while run_time < end_time:
-            # Read the imput buffer
+        # b, a = signal.butter(order, freq, btype = filter_type, analog=False)
+        output = signal.lfilter(b, a, pre_buffer)
+        # print time.time() - timing
+        # return output[-self.period_size:]
+        return output[len(output)/2:]
+
+
+    def rt_lfilter(self, b, a, run_time):
+        """ Realtime lfilter
+        Execute a filter over the audio captured with the microfone.
+
+        b: 
+        a: 
+        run_time: Time to run filter in seconds
+        """
+        start_time = time.time()
+        comp_time = 0
+        prev_buffer = [0.0] * self.period_size
+        while time.time() - start_time < run_time:
+
+            # Empty everything in the buffer that was recorded during the 
+            # Computations.
+            buffer = ""
+            length = 0
+            while comp_time > self.period_size / float(self.frame_rate)::
+                l,date = self.inp.read()
+                comp_time -= self.period_size / float(self.frame_rate)
+                buffer = buffer + data
+                length = length + l
+
+            # Fetch the final buffer
             l, data = self.inp.read()
 
-            # Convert to an array of floats
-            floats = struct.unpack('f' * self.period_size, data)
+            # Get the start time of the computational process. 
+            start_time = time.time()
+            
+            # Unpack the buffer
+            floats = struct.unpack('f' * (length + l), buffer + data)
 
+            # Subsample buffer to self.period_size
+            floats = floats[0::len(floats)/self.period_size]
 
-            result = self.foqaps_butter(5, .03, floats)
-            #
-            # HERE IS SPACE TO MANIPULATE AUDIO
-            #
+            # Execute the filter over buffer. 
+            result = self.execute_filter(b, a, floats, prev_buffer)
 
             # Convert floats back to a struct for output.
-            test = struct.pack('f' * self.period_size, *result)
+            output = struct.pack('f' * l, *result)
+
+            # Calculate the computation time filter. 
+            comp_time = time.time() - start_time
 
             # Output audio
-            self.out.write(test)
+            self.out.write(output)
 
-            run_time += 1
+            # Keep the buffer to use as samples for the next buffer.
+            prev_buffer = floats
 
 
 
 if __name__ == '__main__':
+    # Build the FOQAPS object. setting up variables and the output stream
     foqaps = FOQAPS()
 
-    foqaps.playback_audio_realtime(1000000)
+    order = 4
+    freq = .03
+
+    # if type(freq) is int:
+    #     freq = freq / (float(self.frame_rate) / 2)
+
+    # Determine the Numerator and denomenator. 
+    b, a = signal.butter(order, freq, btype = 'lowpass', analog=False)
+
+    # Execture the filter in realtime
+    foqaps.rt_lfilter(b, a, 1000)
