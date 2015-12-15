3d83144cc407991ed50281b8df6ae4fdc16199ef
Braamling <contact@bramvandenakker.nl>
2015-12-15 12:46:02 +0100

    I HAVE NO CLUE WHAT I'M DOING

--- FOQAPS/test.py
+++ FOQAPS/test.py
@@ -1,4 +1,5 @@
-import alsaaudio, sys, audioop
+import alsaaudio
+import struct
 import time
 import scipy
  
@@ -8,10 +9,10 @@ class FOQAPS():
     def __init__(self):
         channels = 1
         sample_size = 1
-        frame_size = channels * sample_size
+        self.frame_size = channels * sample_size
         frame_rate = 44100
-        byte_rate = frame_rate * frame_size
-        period_size = 160
+        byte_rate = frame_rate * self.frame_size
+        self.period_size = 160
 
         # Create output object
         self.out = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK, alsaaudio.PCM_NORMAL)
@@ -20,7 +21,7 @@ class FOQAPS():
         self.out.setchannels(channels)
         self.out.setformat(alsaaudio.PCM_FORMAT_FLOAT_LE)
         self.out.setrate(frame_rate)
-        self.out.setperiodsize(period_size)
+        self.out.setperiodsize(self.period_size)
 
         # Create input object
         self.inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL)
@@ -29,7 +30,7 @@ class FOQAPS():
         self.inp.setchannels(channels)
         self.inp.setrate(frame_rate)
         self.inp.setformat(alsaaudio.PCM_FORMAT_FLOAT_LE)#16 bit little endian
-        self.inp.setperiodsize(period_size)
+        self.inp.setperiodsize(self.period_size)
          
          
     def foqaps_butter(self, order, freq , data, filter_type = 'lowpass'):
@@ -44,13 +45,29 @@ class FOQAPS():
     def playback_audio_realtime(self, end_time):
         run_time = 0
         while run_time < end_time:
-            l, data = self.inp.read()#l seems to always be 940
-            self.out.write(data)
-            time.sleep(.001)
+            # Read the imput buffer
+            l, data = self.inp.read()
+
+            # Convert to an array of floats
+            floats = struct.unpack('f' * self.period_size, data)
+
+            #
+            # HERE IS SPACE TO MANIPULATE AUDIO
+            #
+            win = pg.GraphicsWindow()  # Automatically generates grids with multiple items
+            win.addPlot(floats, row=0, col=0)
+
+            # Convert floats back to a struct for output.
+            test = struct.pack('f' * self.period_size, *floats)
+
+            # Output audio
+            self.out.write(test)
 
             run_time += 1
 
 
+
 if __name__ == '__main__':
     foqaps = FOQAPS()
+
     foqaps.playback_audio_realtime(1000000)
